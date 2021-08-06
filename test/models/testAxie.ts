cebe6ea1df1e44062a31c086b0cd6278208e4f88
楊宗翰 <feveraly@gmail.com>
2021-08-07 01:09:52 +0800

    add mocha test

---
+++ test/models/testAxie.ts
@@ -0,0 +1,5 @@
+import * as assert from "assert";
+import * as chai from "chai";
+
+
+import {Axie, AxieStage, AxieAuction} from '../../src/models/axie'
---
+++ test/models/testPrice.ts
@@ -0,0 +1,12 @@
+import * as assert from "assert";
+import {expect} from "chai";
+
+import Price from '../../src/models/price'
+
+describe('models/price', () => {
+
+    it('_toString should xxx', () => {
+        expect('kqopwe').to.be.a('string');
+        expect('qjwope').to.equal('qjwope')
+    })
+})
