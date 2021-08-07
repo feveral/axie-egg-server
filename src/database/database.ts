ff462ac591e87dc6091b653d0616944f0b279eea
楊宗翰 <feveraly@gmail.com>
2021-08-08 00:00:29 +0800

    add mongo DB

---
+++ src/database/database.ts
@@ -0,0 +1,66 @@
+import { Collection, Db, MongoClient } from "mongodb";
+
+const mongo = require('mongodb').MongoClient;
+import config from '../config'
+
+class Database {
+
+    private _dbName: string
+    private _client: MongoClient
+    private _db: Db
+
+    constructor (dbName: string) {
+        this.databaseName = dbName;
+    }
+
+    set databaseName (value: string) {
+        this._dbName = value;
+        this._client = undefined;
+        this._db = undefined;
+        this.getDB();
+    }
+
+    async getClient (): Promise<MongoClient> {
+        if (this._client === undefined) {
+            try {
+                this._client = await MongoClient.connect(config.mongoUrl);
+                return this._client;
+            } catch (error) {
+                console.log(`connnected ${config.mongoUrl} error`);
+                console.log(`error: ${error}`);
+            }            
+        }
+        return this._client;
+    }
+
+    async getDB(): Promise<Db> {
+        if (this._client === undefined || this._db === undefined) {
+            try {
+                this._client = await MongoClient.connect(config.mongoUrl);
+                this._db = await this._client.db(this._dbName);
+                return this._db;
+            } catch (error) {
+                console.log(`connnected ${config.mongoUrl} error`);
+                console.log(`error: ${error}`);
+            }
+        }
+        return this._db;
+    }
+    
+    async getCollection (collectionName: string): Promise<Collection> {
+        if (this._client === undefined || this._db === undefined) {
+            await this.getDB();
+        }
+        const collection = this._db.collection(collectionName);
+        return collection;
+    }
+
+    async closeDB (): Promise<void> {
+        await this._client.close();
+        this._client = undefined;
+        this._db = undefined;
+    }
+}
+
+const database = new Database(config.mongoDatabaseName);
+export default database;
